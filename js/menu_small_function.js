a15abc64a5267c6540e0d5e9eab467cb34080a50
Domesticus6 <40352609+Domesticus6@users.noreply.github.com>
2019-10-06 21:15:51 +0300

    Dinamo Riga site

---
+++ js/menu_small_function.js
@@ -0,0 +1,15 @@
+function openNav() {
+	document.getElementById("myNav")
+	.style.height="100%";
+	document.getElementById('strelka').style.display = "none";
+	document.getElementById('carousel-indicatorss').style.display = "none";
+	document.getElementById('carousel').style.display = "none";
+}
+
+function closeNav() {
+	document.getElementById("myNav")
+	.style.height = "0%";
+	document.getElementById('strelka').style.display = "block";
+	document.getElementById('carousel-indicatorss').style.display = "block";
+	document.getElementById('carousel').style.display = "block";
+}
